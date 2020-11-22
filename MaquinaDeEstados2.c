f7d0cb6ecd6b9aed589f057b70966c5e1cec5782
OscarBaez <oscare.baez@edu.uag.mx>
2020-11-21 23:30:44 -0600

    maquinas de estado Oscar

---
+++ MaquinaDeEstados2.c
@@ -0,0 +1,328 @@
+#include "stdio.h"
+#include "MaquinaDeEstados2.h"
+#include "stdlib.h"
+
+
+void main (void)
+{
+  unsigned int u8Delay = 0;
+  while(vaso == 1)
+  {
+  system("cls");
+  printf ("Bienvenido a la Maquina de bebidas\n");
+  printf ("Desea hielos en su bebida?\n");
+  printf ("1) Si   2) No\n");
+  scanf ("%d", &hielo);
+  if (hielo == 1)
+    {
+      printf ("Vaso desplazandose\n");
+      u8Delay = hieloTime;
+      while(u8Delay--);
+      printf("Sirviendo hielos a la bebida\n");
+      u8Delay = inicioTime;
+      while(u8Delay--);
+      printf("Vaso dezplazandose al inicio\n\n");
+      u8Delay = inicioTime;
+      while(u8Delay--);
+    }
+  else
+    {
+      //no hace nada
+    }
+  printf("A continuacion se presenta el menu de las bebidas\n");
+  u8Delay = inicioTime;
+  while(u8Delay--);
+  printf ("-------------Menu-------------\n");
+  printf ("1) $20 - CocaCola\n");
+  printf ("2) $35 - Whisky\n");
+  printf ("3) $25 - Vodka\n");
+  printf ("4) $15 - Sprite\n");
+  printf ("5) $10 - Lima\n");
+  printf ("6) $30 - Ron\n");
+  printf ("7) $50 - Jarabe\n");
+  printf ("Seleccione el primer ingrediente de la bebida\n");
+  scanf  ("%d", &bebida);
+  printf ("Seleccione el segundo ingrediente de la bebida\n");
+  scanf  ("%d", &bebida1);
+  printf ("Seleccione el tercer ingrediente de la bebida\n");
+  scanf  ("%d", &bebida2);
+  printf ("Seleccione el cuarto ingrediente de la bebida\n");
+  scanf  ("%d", &bebida3);
+  printf ("Iniciando...\n\n");
+  u8Delay = inicioTime;
+  while(u8Delay--);
+switch (bebida)
+    {
+    case 1:
+      printf ("Vaso desplazandose\n\n");
+        CocaCola = preciobebida + PrecioCocaCola;
+        u8Delay = CocaColaTime;
+        while(u8Delay--);
+        printf ("Sirviendo CocaCola\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 2:
+      printf ("Vaso desplazandose\n\n");
+        Whisky = preciobebida + PrecioWhisky;
+        u8Delay = WhiskyTime;
+        while(u8Delay--);
+        printf ("Sirviendo Whisky\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 3:
+      printf ("Vaso desplazandose\n\n");
+        Vodka = preciobebida + PrecioVodka;
+        u8Delay = VodkaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Vodka\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 4:
+      printf ("Vaso desplazandose\n\n");
+        Sprite = preciobebida + PrecioSprite;
+        u8Delay = SpriteTime;
+        while(u8Delay--);
+        printf ("Sirviendo Sprite\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 5:
+      printf ("Vaso desplazandose\n\n");
+        Lima = preciobebida + PrecioLima;
+        u8Delay = LimaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Lima\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 6:
+      printf ("Vaso desplazandose\n\n");
+        Ron = preciobebida + PrecioRon;
+        u8Delay = RonTime;
+        while(u8Delay--);
+        printf ("Sirviendo Ron\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 7:
+      printf ("Vaso desplazandose\n\n");
+        Jarabe = preciobebida + PrecioJarabe;
+        u8Delay = JarabeTime;
+        while(u8Delay--);
+        printf ("Sirviendo Jarabe\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    }
+switch (bebida1)
+    {
+    case 1:
+      printf ("Vaso desplazandose\n\n");
+        CocaCola = preciobebida + PrecioCocaCola;
+        u8Delay = CocaColaTime;
+        while(u8Delay--);
+        printf ("Sirviendo CocaCola\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 2:
+      printf ("Vaso desplazandose\n\n");
+        Whisky = preciobebida + PrecioWhisky;
+        u8Delay = WhiskyTime;
+        while(u8Delay--);
+        printf ("Sirviendo Whisky\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 3:
+      printf ("Vaso desplazandose\n\n");
+        Vodka = preciobebida + PrecioVodka;
+        u8Delay = VodkaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Vodka\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 4:
+      printf ("Vaso desplazandose\n\n");
+        Sprite = preciobebida + PrecioSprite;
+        u8Delay = SpriteTime;
+        while(u8Delay--);
+        printf ("Sirviendo Sprite\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 5:
+      printf ("Vaso desplazandose\n\n");
+        Lima = preciobebida + PrecioLima;
+        u8Delay = LimaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Lima\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 6:
+      printf ("Vaso desplazandose\n\n");
+        Ron = preciobebida + PrecioRon;
+        u8Delay = RonTime;
+        while(u8Delay--);
+        printf ("Sirviendo Ron\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 7:
+      printf ("Vaso desplazandose\n\n");
+        Jarabe = preciobebida + PrecioJarabe;
+        u8Delay = JarabeTime;
+        while(u8Delay--);
+        printf ("Sirviendo Jarabe\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    }
+switch (bebida2)
+    {
+    case 1:
+      printf ("Vaso desplazandose\n\n");
+        CocaCola = preciobebida + PrecioCocaCola;
+        u8Delay = CocaColaTime;
+        while(u8Delay--);
+        printf ("Sirviendo CocaCola\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 2:
+      printf ("Vaso desplazandose\n\n");
+        Whisky = preciobebida + PrecioWhisky;
+        u8Delay = WhiskyTime;
+        while(u8Delay--);
+        printf ("Sirviendo Whisky\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 3:
+      printf ("Vaso desplazandose\n\n");
+        Vodka = preciobebida + PrecioVodka;
+        u8Delay = VodkaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Vodka\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 4:
+      printf ("Vaso desplazandose\n\n");
+        Sprite = preciobebida + PrecioSprite;
+        u8Delay = SpriteTime;
+        while(u8Delay--);
+        printf ("Sirviendo Sprite\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 5:
+      printf ("Vaso desplazandose\n\n");
+        Lima = preciobebida + PrecioLima;
+        u8Delay = LimaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Lima\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 6:
+      printf ("Vaso desplazandose\n\n");
+        Ron = preciobebida + PrecioRon;
+        u8Delay = RonTime;
+        while(u8Delay--);
+        printf ("Sirviendo Ron\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 7:
+      printf ("Vaso desplazandose\n\n");
+        Jarabe = preciobebida + PrecioJarabe;
+        u8Delay = JarabeTime;
+        while(u8Delay--);
+        printf ("Sirviendo Jarabe\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    }
+switch (bebida3)
+    {
+    case 1:
+      printf ("Vaso desplazandose\n\n");
+        CocaCola = preciobebida + PrecioCocaCola;
+        u8Delay = CocaColaTime;
+        while(u8Delay--);
+        printf ("Sirviendo CocaCola\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 2:
+      printf ("Vaso desplazandose\n\n");
+        Whisky = preciobebida + PrecioWhisky;
+        u8Delay = WhiskyTime;
+        while(u8Delay--);
+        printf ("Sirviendo Whisky\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 3:
+      printf ("Vaso desplazandose\n\n");
+        Vodka = preciobebida + PrecioVodka;
+        u8Delay = VodkaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Vodka\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 4:
+      printf ("Vaso desplazandose\n\n");
+        Sprite = preciobebida + PrecioSprite;
+        u8Delay = SpriteTime;
+        while(u8Delay--);
+        printf ("Sirviendo Sprite\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 5:
+      printf ("Vaso desplazandose\n\n");
+        Lima = preciobebida + PrecioLima;
+        u8Delay = LimaTime;
+        while(u8Delay--);
+        printf ("Sirviendo Lima\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 6:
+      printf ("Vaso desplazandose\n\n");
+        Ron = preciobebida + PrecioRon;
+        u8Delay = RonTime;
+        while(u8Delay--);
+        printf ("Sirviendo Ron\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    case 7:
+      printf ("Vaso desplazandose\n\n");
+        Jarabe = preciobebida + PrecioJarabe;
+        u8Delay = JarabeTime;
+        while(u8Delay--);
+        printf ("Sirviendo Jarabe\n\n");
+        u8Delay = inicioTime;
+        while(u8Delay--);
+      break;
+    }
+      printf ("Vaso dezplazandose al inicio\n");
+      u8Delay = inicioTime;
+      while(u8Delay--);
+      printf ("Su bebida esta lista\n");
+      precio = CocaCola+Whisky+Vodka+Sprite+Lima+Ron+Jarabe;
+      printf ("El precio de su bebida es %d \n\n", precio);
+      printf ("Desea otra bebida?\n");
+      printf ("1) Si   2) No\n");
+      scanf ("%d", &vaso);
+}
+}
---
+++ MaquinaDeEstados2.h
@@ -0,0 +1,41 @@
+
+#define CocaColaTime 987654321
+#define WhiskyTime   987654321
+#define VodkaTime    987654321
+#define SpriteTime   987654321
+#define LimaTime     987654321
+#define RonTime      987654321
+#define JarabeTime   987654321
+#define hieloTime    987654321
+#define inicioTime   987654321
+
+typedef unsigned char 	uint8; 
+typedef unsigned short 	uint16; 
+typedef unsigned int 	uint32;
+
+uint8 hielo;
+uint8 bebida         = 0;
+uint8 bebida1        = 0;
+uint8 bebida2        = 0;
+uint8 bebida3        = 0;
+uint8 precio         = 0;
+uint8 CocaCola       = 0;
+uint8 Whisky         = 0; 
+uint8 Vodka          = 0; 
+uint8 Sprite         = 0; 
+uint8 Lima           = 0; 
+uint8 Ron            = 0;
+uint8 Jarabe         = 0;
+uint8 vaso           = 1;
+uint8 preciobebida   = 0;
+typedef enum
+{
+	PrecioCocaCola = 20,
+	PrecioWhisky   = 35, 
+	PrecioVodka    = 25, 
+	PrecioSprite   = 15, 
+	PrecioLima     = 10, 
+	PrecioRon      = 30,
+	PrecioJarabe   = 50,
+
+}SM_tenStates;
